3736da41a895486c23e06a7e6fd3157aca6eb994
Edgar Benavides <edgarben27@gmail.com>
2025-02-05 13:24:36 -0600

    feat: Add table

--- index.js
+++ index.js
@@ -1,23 +1,61 @@
 const $ = (id) => document.getElementById(id);
 
+function taskListItem(task) {
+  return `<tr class="bg-white border-b border-gray-200 hover:bg-gray-50">
+    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">${
+      task.title
+    }</th>
+    <td class="px-6 py-4">${task.notes}</td>
+    <td class="px-6 py-4">${new Date(task.start).toLocaleTimeString("es-MX", {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: false,
+    })}</td>
+    <td class="px-6 py-4">${new Date(task.end).toLocaleTimeString("es-MX", {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: false,
+    })}</td>
+  </tr>`;
+}
+
 function getCurrentTasks() {
   const tasks = JSON.parse(window.localStorage.getItem("tasks") ?? "[]");
 
   return tasks;
 }
 
-function showCurrentTasks() {
-  const container = $("currentTasksList");
-
+function getTotalHours() {
   const currentTasks = getCurrentTasks();
 
+  const totalMinutes = currentTasks.reduce((sum, task) => {
+    return sum + (task.end - task.start) / (1000 * 60);
+  }, 0);
+
+  const hours = String(Math.floor(totalMinutes / 60)).padStart(2, "0");
+  const minutes = String(Math.round(totalMinutes % 60)).padStart(2, "0");
+
+  return `${hours}:${minutes}`;
+}
+
+function showCurrentTasks() {
+  const container = $("currentTaskTable");
+
   if (container) {
-    currentTasks.forEach((task, index) => {
-      const li = document.createElement("li");
-      li.setAttribute("id", index);
-      li.innerText = task.title;
-      container.appendChild(li);
+    const currentTasks = getCurrentTasks();
+    container.innerHTML = "";
+    currentTasks.forEach((task) => {
+      container.innerHTML += taskListItem(task);
     });
+  }
+}
+
+function showTotalHours() {
+  const totalHoursContainer = $("totalHoursContainer");
+
+  if (totalHoursContainer) {
+    const totalHours = getTotalHours();
+    totalHoursContainer.innerText = totalHours;
   }
 }
 
@@ -31,26 +69,37 @@ function main() {
 
     const values = Object.fromEntries(formData);
 
-    const { title, start, end } = values;
+    const { title, start, end, notes } = values;
 
     if (!title || !start || !end) {
       return;
     }
 
+    console.log({ start, end });
+
     const currentTasks = JSON.parse(
       window.localStorage.getItem("tasks") ?? "[]",
     );
 
-    const newTasks = [...currentTasks, { ...values }];
+    const newTask = {
+      title,
+      start: new Date().setHours(start.split(":")[0], start.split(":")[1]),
+      end: new Date().setHours(end.split(":")[0], end.split(":")[1]),
+      notes,
+    };
+
+    const newTasks = [...currentTasks, newTask];
 
     window.localStorage.setItem("tasks", JSON.stringify(newTasks));
 
     event.target.reset();
 
     showCurrentTasks();
+    showTotalHours();
   });
 
   showCurrentTasks();
+  showTotalHours();
 }
 
 main();
